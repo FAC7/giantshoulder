1ddfc021538c3fc4a62e502eff2858c9a930115f
Elias <elias0789@gmail.com>
2016-05-06 14:56:54 +0100

    working status

--- imports/ui/Chatbox/index.jsx
+++ imports/ui/Chatbox/index.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 export default class Chatbox extends React.Component {
   componentDidMount () {
     document.getElementsByClassName('chatbox')[0].addEventListener('keydown', (e) => {
-      if (e.which === 13) e.preventDefault()
+      if (e.which === 13) {
+        e.preventDefault()
+        this.onSubmit()
+      }
     })
+  }
+
+  onClick (e) {
+    e.preventDefault()
+    const el = e.target
+    el.innerHTML = el.innerHTML === 'Chat with peers' ? 'Ask a question' : 'Chat with peers'
+    el.className = el.className === 'chat-btn chat' ? 'chat-btn chat' : 'chat-btn question'
   }
 
   onSubmit () {
@@ -15,8 +25,14 @@ export default class Chatbox extends React.Component {
     return (
       <form onSubmit={this.onSubmit}>
         <div className='container'>
-          <textarea className='chatbox inline' type='text' placeholder='message here...' />
-          <button className='chat-btn question' type='button'>Q</button>
+          <textarea className='chatbox inline' type='text' placeholder='Chat here...' />
+          <button
+            onClick={this.onClick}
+            className='chat-btn question'
+            type='button'
+          >
+            Ask a question
+          </button>
         </div>
       </form>
     )
